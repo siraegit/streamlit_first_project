4a0975e1e38e14776b4d0f554e5d6a90db27d2b6
siraegit <siraegit@gmail.com>
2026-08-18 10:52:53 +0900

    Update 02_오늘의급식B.py
    타이틀 수정

--- pages/02_오늘의급식B.py
+++ pages/02_오늘의급식B.py
@@ -4,4 +4,4 @@ import utils
 office_code = st.secrets["school_B"]["education_office_code"]
 school_code = st.secrets["school_B"]["school_code"]
 
-utils.render_meal_page("[YG]오늘의 급식", office_code, school_code)
+utils.render_meal_page("[YG]", office_code, school_code)
