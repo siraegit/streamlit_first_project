445534fa6965ff950e488bc71cda05481646a00d
siraegit <siraegit@gmail.com>
2026-08-18 10:53:22 +0900

    Update 01_오늘의급식A.py

--- pages/01_오늘의급식A.py
+++ pages/01_오늘의급식A.py
@@ -4,4 +4,4 @@ import utils
 office_code = st.secrets["school_A"]["education_office_code"]
 school_code = st.secrets["school_A"]["school_code"]
 
-utils.render_meal_page("BS", office_code, school_code)
+utils.render_meal_page("[BS]", office_code, school_code)
